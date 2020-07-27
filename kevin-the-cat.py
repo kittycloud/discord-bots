1d71579b711c5e99707992b491cee08fa3de2506
Joe Stafford <joe.stafford@slalom.com>
2020-07-27 15:13:16 -0400

    tweaks

--- kevin-the-cat.py
+++ kevin-the-cat.py
@@ -15,7 +15,8 @@ async def on_message(message):
         return
 
     mention = message.mentions
-    if (client.user in mention) and (message.content.contains("hi")):
-       await message.channel.send('🎶 Hi! My name is {0.user.name}, I am an orange cat, and I have come here.. to help make shopping lists! 🎶'.format(client))
+    if client.user in mention:
+        if 'hi' in message.content.lower():
+            await message.channel.send('🎶 Hi! My name is {0.user.name}, I am an orange cat, and I have come here.. to help make shopping lists! 🎶'.format(client))
 
 client.run(TOKEN)
