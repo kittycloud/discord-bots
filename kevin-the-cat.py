f56d01242536268ff0e039bfc4b308119eaa2a11
Joe <joestafford045@gmail.com>
2020-07-28 21:14:55 +0000

    send message

--- kevin-the-cat.py
+++ kevin-the-cat.py
@@ -25,6 +25,7 @@ async def on_message(message):
         if 'play' in message.content.lower():
             guild = message.guild
             channel = await guild.create_text_channel('kevins-funhouse')
+            await channel.send('my name is kevin!')
             # time.sleep(15)
             # await channel.delete()
 
