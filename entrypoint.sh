7c34050a1ab8176f7048fe0e62ab16e91554efb3
Joe <joestafford045@gmail.com>
2020-07-28 01:28:37 +0000

    update git url

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-git clone --depth 1 git@github.com:kittycloud/discord-bots.git -- kevin-the-cat.py
+git clone --depth 1 https://github.com/kittycloud/discord-bots.git -- kevin-the-cat.py
 /usr/local/bin/python3 kevin-the-cat.py
