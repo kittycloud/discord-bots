1ad077de58f22f59a6a6259e11f6ee2b635f83e7
Joe <joestafford045@gmail.com>
2020-07-28 01:43:35 +0000

    revert entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-echo yes | git clone --depth 1 https://github.com/kittycloud/discord-bots.git -- kevin-the-cat.py
-/usr/local/bin/python3 kevin-the-cat.py
+curl https://raw.githubusercontent.com/kittycloud/discord-bots/master/kevin-the-cat.py --output /app/main.py
+/usr/local/bin/python3 /app/main.py
